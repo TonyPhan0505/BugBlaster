69b2175f909018738de00913e96410d7b141a99a
B Phan <bphan@Binh.local>
2024-01-19 20:30:08 -0700

    added pages

---
+++ backend/models/bug.model.js
@@ -0,0 +1,47 @@
+////////////// Import dependencies ///////////////
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+////////////////////////////////////////////////
+
+////////////// Database schema /////////////
+const BugSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    datetime: {
+        type: Date,
+        required: true
+    },
+    briefDescription: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    detailedDescription: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    assignees: {
+        type: [String],
+        required: true
+    },
+    updates: {
+        type: [String],
+        required: true,
+        default: []
+    },
+    solution: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+///////////////////////////////////////////
+
+////////////////////// Exports ////////////////////
+module.exports = mongoose.model('Bug', BugSchema);
+////////////////////////////////////////////////////
---
+++ backend/models/update.model.js
@@ -0,0 +1,33 @@
+////////////// Import dependencies ///////////////
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+////////////////////////////////////////////////
+
+////////////// Database schema /////////////
+const UpdateSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    datetime: {
+        type: Date,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    location: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+///////////////////////////////////////////
+
+////////////////////// Exports ////////////////////
+module.exports = mongoose.model('Update', UpdateSchema);
+////////////////////////////////////////////////////
